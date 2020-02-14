7da273f7afa681606ef7ca4572964fea436b9c87
Gülenay Gül <gulenay_gul@kocsistem.com.tr>
2020-02-14 18:43:58 +0300

    Initial commit

---
+++ PopTV/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  PopTV
+//
+//  Created by Gülenay Gül on 14.02.2020.
+//  Copyright © 2020 glnygl. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
